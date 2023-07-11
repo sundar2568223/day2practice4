543e1ac8dd4e86573c6efa0b0c36b488f63d210e
sunder2568223 <sunderyssr@gmail.com>
2023-07-11 12:23:08 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
+using System.Text;
+
 namespace Consoleday2prac4
 {
     internal class program
     {
         static void Main(string[] args)
         {
-            string userName;
-            Again:
-            Console.WriteLine("Enter User Name");
-            userName = Console.ReadLine();
-            if (userName.Length >= 6)
+            //string userName;
+            //Again:
+            //Console.WriteLine("Enter User Name");
+            //userName = Console.ReadLine();
+            //if (userName.Length >= 6)
+            //{
+            //    Console.WriteLine("Welcome : \t " + userName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid User Name");
+            //    goto Again;
+            //}
+            //Console.ReadKey();
+            //string mystring = "sundar";
+            //StringBuilder stringBuilder = new StringBuilder("welcome");
+            //stringBuilder.Append("To C#");
+            //stringBuilder.Append("I am String Builder");
+            //stringBuilder.Append("string:\t " + mystring);
+            //Console.WriteLine(stringBuilder);
+            //Console.ReadKey();
+            for(int i = 1;i<10;i++)
             {
-                Console.WriteLine("Welcome : \t " + userName);
+                if(i == 6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value:{0}", i);
             }
-            else
-            {
-                Console.WriteLine("Invalid User Name");
-                goto Again;
-            }
-            Console.ReadKey();
+        end: Console.WriteLine("The end");
+       
         }
 
     }
