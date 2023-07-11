8c5a297d5ba5220f238a469b5891d32001490036
sunder2568223 <sunderyssr@gmail.com>
2023-07-11 11:58:26 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Consoleday2prac4
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Again:
+            Console.WriteLine("Enter User Name");
+            userName = Console.ReadLine();
+            if (userName.Length >= 6)
+            {
+                Console.WriteLine("Welcome : \t " + userName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid User Name");
+                goto Again;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
